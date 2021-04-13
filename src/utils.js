cc100730e7c544bc9f2348d0bbd5abf63d04089a
yahelbn <yahelbn3123@gmail.com>
2021-04-13 12:28:11 +0300

    update build

--- src/utils.js
+++ src/utils.js
@@ -145,12 +145,12 @@ export function numberToEnglish(n, custom_join_character) {
     }
   }
 
-  /* Take Care to numbers size=1 */
+  /* handle Care to numbers size=1 */
   if (string.length === 1) {
     words.splice(1, 1);
   }
 
-  /* Take Care to numbers size=2 */
+  /* handle to numbers size=2 */
   if (string.length === 2) {
     if (words.length === 4 && words.includes("-")) {
       words.splice(3, 1);
